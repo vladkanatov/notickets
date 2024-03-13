f8aef6fc8a74414d0071f154d6367b7fe24f1d2a
lon8 <vlad.kanatik@ya.ru>
2024-03-13 18:11:07 +0300

    rework parser.py

--- parser.py
+++ parser.py
@@ -1,19 +1,11 @@
 from datamining.module.controller import Parser
 from datamining.module.logger import logger
-from bs4 import BeautifulSoup
 
 
-class Kek(Parser):
+class ParserName(Parser):
     def __init__(self):
         super().__init__()
-
     async def main(self):
-        response = await self.session.get('https://mossoveta.ru/timetable/')
+        logger.info('Hello, parser')
 
-        soup = BeautifulSoup(response.text, 'lxml')
-
-        divs = soup.find_all('div', class_='title')
-
-        for div in divs:
-            logger.debug(div.text)
-
+        # self.register_event()
