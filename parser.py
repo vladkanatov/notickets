47c218bca1acbddd720221198baa615777fb9f45
lon8 <vlad.kanatik@ya.ru>
2024-03-13 14:51:30 +0300

    delete self.name from parser.py

--- parser.py
+++ parser.py
@@ -5,9 +5,6 @@ from datamining.module.logger import logger
 class ParserName(Parser):
     def __init__(self):
         super().__init__()
-        # Имя парсера (лучше указать адрес, как на примере ниже)
-        self.name = 'bileter_ru'
-
     async def main(self):
         logger.info('Hello, parser')
 
