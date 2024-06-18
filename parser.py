8d237b0229dc43eef2c617f3b596a872ce59d57f
Vladislav <kanatiklon@gmail.com>
2024-06-18 03:22:22 +0300

    good

--- parser.py
+++ parser.py
@@ -1,110 +1,57 @@
-from datetime import datetime
-from typing import Optional
-from bs4 import BeautifulSoup
-
 from datamining.module.controller import Parser
 from datamining.module.logger import logger
+from bs4 import BeautifulSoup
 
+from datetime import datetime
 
-class Bileter(Parser):
+class Template(Parser):
+
     def __init__(self):
         super().__init__()
-        # Имя парсера
-        self.name = 'bileter_ru'
-
-        self.delay = 1800
-        self.driver_source = None
-        self.headers = {
-            'authority': 'www.bileter.ru',
-            'accept': 'text/html, */*; q=0.01',
-            'accept-language': 'ru,en;q=0.9',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'referer': 'https://www.bileter.ru/afisha',
-            'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "YaBrowser";v="23"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Linux"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': self.user_agent,
-            'x-pjax': 'true',
-            'x-pjax-container': '#js_id_afisha_performances_grid',
-            'x-requested-with': 'XMLHttpRequest',
-        }
-        self.urls = [
-            # Москва
-            'https://msk.bileter.ru/afisha?building[]=1930_moscow.html',
-
-            # СПБ
-            # 'https://www.bileter.ru/afisha?building[]=aleksandrinskiy_teatr.html', # Александрийский театр
-            # 'https://www.bileter.ru/afisha?building[]=bolshoy_dramaticheskiy_teatr_imgatovstonogova.html', # БДТ Товстогонова
-            'https://www.bileter.ru/afisha?building[]=dom_ofitserov.html',  # Дом офицеров
-            'https://www.bileter.ru/afisha?building[]=bolshoy_kontsertnyiy_zal_oktyabrskiy.html'  # БКЗ "Октябрьский"
-        ]
-
+        
     @staticmethod
-    def reformat_venue(venue: str) -> Optional[str]:
-        return {
-            'Александрийский театр': 'Александрийский театр!'
-        }.get(venue, None)
+    def convert_to_datetime(date_str: str, time_str: str) -> str:
+        try:
+            # Словарь для преобразования названий месяцев на русском в числовой формат
+            month_names = {
+                "января": "01",
+                "февраля": "02",
+                "марта": "03",
+                "апреля": "04",
+                "мая": "05",
+                "июня": "06",
+                "июля": "07",
+                "августа": "08",
+                "сентября": "09",
+                "октября": "10",
+                "ноября": "11",
+                "декабря": "12"
+            }
 
-    @staticmethod
-    def month_string_to_number(month):
-        return {
-            'Января': 1, 'Февраля': 2, 'Марта': 3, 'Апреля': 4,
-            'Мая': 5, 'Июня': 6, 'Июля': 7, 'Августа': 8,
-            'Сентября': 9, 'Октября': 10, 'Ноября': 11, 'Декабря': 12
-        }[month]
+            # Разбиваем строку с датой на день и месяц
+            day, month_name = date_str.split()
+            
+            # Преобразуем месяц в числовой формат
+            month = month_names[month_name]
+            
+            # Получаем текущий год
+            current_year = datetime.now().year
+            
+            # Собираем дату в формате ГГГГ-ММ-ДД
+            date_formatted = f"{current_year}-{month}-{day.zfill(2)}"
+            
+            # Собираем полную дату и время
+            datetime_formatted = f"{date_formatted} {time_str}"
+            
+            cleaned_string = datetime_formatted.split('\t')[0]
 
-    def str_to_datetime(self, date: str) -> datetime:
-        date_parts = date.split()
-        # Извлекаем день, месяц и время
-        day = date_parts[0]
-        if "Открытая дата" in date:  # Если точной даты не существуеты
+# Преобразуем строку в объект datetime
+            datetime_obj = datetime.strptime(cleaned_string, '%Y-%m-%d %H:%M')
+            
+            return datetime_obj
+        except ValueError as e:
+            print(f"Ошибка преобразования даты и времени: {e}")
             return None
-        day = int(day)
-        month_string = date_parts[1].replace(',', '')
-        month_number = self.month_string_to_number(month_string)
-        time = date_parts[-1]
-        year = date_parts[-2].replace(',', '')
-        if month_string == year:  # Если не указан год
-            year = datetime.now().year
-        # Собираем все в datetime объект
-        final_date = datetime(year=int(year), month=month_number, day=day, hour=int(time.split(':')[0]),
-                              minute=int(time.split(':')[1]))
-        return final_date
-
-    def get_date(self, event: BeautifulSoup):
-        date = event.find('div', class_='date').text
-        datetime_date = self.str_to_datetime(date)
-        return datetime_date
-
-    @staticmethod
-    def get_link(event: BeautifulSoup) -> str:
-        price_block = event.find('div', class_='price')
-        link = "https://www.bileter.ru" + price_block.find('a')['href']
-        return link
-
-    def get_date_from_list(self, event: BeautifulSoup) -> datetime:
-        date_and_price = event.find('a')
-        date = date_and_price.contents[0]
-        datetime_date = self.str_to_datetime(date)
-        return datetime_date
-
-    @staticmethod
-    def get_link_from_list(event: BeautifulSoup) -> str:
-        return "https://www.bileter.ru" + event.find('a')['href']
-
-    async def put_db(self, events: list[tuple]) -> None:
-        for event in events:
-            logger.debug(event)
-            await self.register_event(
-                event_name=event[0],
-                link=event[1],
-                date=event[2],
-                venue=event[3],
-                image_link=event[4]
-            )
 
     async def main(self):
 
@@ -121,8 +68,8 @@ class Bileter(Parser):
             
             for container in containers:
                 title = container.find('img', class_='playbill-el-img-mobile')['alt']
-                image_link = container.find('img', class_='playbill-el-img-mobile')['data-src']
-                link = 'https://bkz.ru/afisha' + container['href']
+                image_link = "https://bkz.ru" + container.find('img', class_='playbill-el-img-mobile')['src']
+                link = 'https://bkz.ru' + container['href']
                 time = container.find('div', class_='playbill-el-text-date')
                 
                 if time:
@@ -139,4 +86,4 @@ class Bileter(Parser):
                         # logger.debug(title)
                         # logger.debug(link)
                         # logger.debug(dt)
-                        await self.register_event(title, link, dt, 'Большой концертный зал "Октябрьский"')
+                        await self.register_event(title, link, dt, 'Большой концертный зал "Октябрьский"', image_link)
